d58ab71828fd90948b66083ac00e3340294e1240
Baku <dcyp@163.com>
2025-03-30 16:20:45 +0800

    update layout

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -29,8 +29,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
+    // Component.Graph(),
+    // Component.DesktopOnly(Component.TableOfContents()),
+    Component.TableOfContents(),
     Component.Backlinks(),
   ],
 }
